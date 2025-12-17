7d121424c575a8aeea60c4ccd9228a9a6d5f6841
Rinaldi Andika <rinaldi0121@gmail.com>
2025-12-17 08:51:15 +0700

    add consultant

--- assets/js/developers.js
+++ assets/js/developers.js
@@ -4,13 +4,13 @@ $(document).ready(function () {
 			const delay = 0.2 + index * 0.1;
 			const ig = dev.instagram.replace('@', '');
 			return `
-				<div class="col-lg-3 col-md-6 mb-4 wow animate__fadeInUp" data-wow-delay="${delay}s">
+				<div class="col-lg-6 col-md-6 mb-4 wow animate__fadeInUp" data-wow-delay="${delay}s">
 					<div class="vs-team vs-team--style2">
 						<div class="vs-team__img">
 							<img src="${dev.image}" alt="${dev.name}">
 						</div>
 						<div class="vs-team__content">
-							<h3 class="vs-team__name">${dev.name}</h3>
+							<h3 class="vs-team__name fs-7">${dev.name}</h3>
 							<span class="vs-team__instagram">${dev.instagram}</span>
 						</div>
 						<ul class="vs-team__share--list">
